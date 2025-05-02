11231f858202d97c78124fae23d37bfd50275cfc
Fagner <fagnergeo0@gmail.com>
2025-05-02 18:28:30 -0300

    feat: add gender selection MVP

---
+++ BMICalculator/Converters/BMIGenderConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator.Converters;
+
+public class BoolToColor : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return (bool)value ? Colors.White : Colors.Gray;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BMICalculator/ViewModels/BMIViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator.ViewModels;
+
+public partial class BMIViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsMaleSelected))]
+    [NotifyPropertyChangedFor(nameof(IsFemaleSelected))]
+    private string? _selectedGender = "Male";
+
+    public bool IsMaleSelected => SelectedGender == "Male";
+    public bool IsFemaleSelected => SelectedGender == "Female";
+
+    [RelayCommand]
+    public void SelectGender(string gender)
+    {
+        SelectedGender = gender; 
+    }
+}
